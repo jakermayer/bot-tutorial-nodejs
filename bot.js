de99242e89930045d09a4d884c2cd3a2ab4efa37
jakermayer <jakemayer8506@gmail.com>
2016-10-10 22:48:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,14 @@ var cool = require('cool-ascii-faces');
 var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
+  var request = JSON.parse(this.req.chunks[0]) +
+                JSON.parse(this.req.chunks[1]) +
+                JSON.parse(this.req.chunks[2]),
       botRegex = /^\/cool guy$/;
 
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text){//&& botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage(request);
     this.res.end();
   } else {
     console.log("don't care");
@@ -18,10 +20,10 @@ function respond() {
   }
 }
 
-function postMessage() {
+function postMessage(message) {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = message;//cool();
 
   options = {
     hostname: 'api.groupme.com',
